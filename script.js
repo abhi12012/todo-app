06baf293260c9cc62ad22c2a937217e9410b66bc
abhi12012 <agrawalabhishek211@gmail.com>
2026-06-03 15:24:37 +0530

    feat: add delete task button

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ function addTask() {
   let input = document.getElementById("taskInput");
   let taskText = input.value;
 
-  // validation
   if (taskText == "") {
     alert("Please enter task!");
     return;
   }
 
   let li = document.createElement("li");
-  li.textContent = taskText;
 
-  // mark complete
-  li.onclick = function () {
-    li.style.textDecoration = "line-through";
+  // Task Text
+  let span = document.createElement("span");
+  span.textContent = taskText;
+
+  span.onclick = function () {
+    span.style.textDecoration = "line-through";
   };
+
+  // Delete Button
+  let delBtn = document.createElement("button");
+  delBtn.textContent = "❌";
+  delBtn.style.marginLeft = "10px";
+  delBtn.style.background = "red";
+  delBtn.style.color = "white";
+  delBtn.style.border = "none";
+  delBtn.style.borderRadius = "5px";
+  delBtn.style.cursor = "pointer";
+
+  delBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(span);
+  li.appendChild(delBtn);
 
   document.getElementById("taskList").appendChild(li);
 
