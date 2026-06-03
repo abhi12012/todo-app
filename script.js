5ce2d60f55b47f47e7021e86df266214c7455028
abhi12012 <agrawalabhishek211@gmail.com>
2026-06-03 11:21:55 +0530

    Fixed todo app bug and UI issue (added missing task list)

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ function addTask() {
   let input = document.getElementById("taskInput");
   let taskText = input.value;
 
+  // validation
+  if (taskText == "") {
+    alert("Please enter task!");
+    return;
+  }
+
   let li = document.createElement("li");
   li.textContent = taskText;
 
+  // mark complete
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+  };
+
   document.getElementById("taskList").appendChild(li);
-}
 
-if (taskText == "") {
-  alert("Please enter task!");
-  return;
+  input.value = "";
 }
-
-li.onclick = function () {
-  li.style.textDecoration = "line-through";
-};
